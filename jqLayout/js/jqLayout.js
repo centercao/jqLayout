4095d0721b7e47a57374b222ba5159ba9ee983dd
centercao <centercao2010@163.com>
2019-01-31 19:19:26 +0800

    完成拖拉改变大小;基本可以使用了.

--- jqLayout/js/jqLayout.js
+++ jqLayout/js/jqLayout.js
@@ -14,89 +14,166 @@
 		// 初始化
 		privateMembers.init(this);
 		// 注册事件
-		// left control bar clicked
-		this.$element.find('.ui-jqLayout-controlBar-left,.ui-jqLayout-controlBar-left > .ui-jqLayout-controlBut').
-		on('click', function () {
-			privateMembers.toggleWest($this,!$(this).hasClass('ui-jqLayout-controlBar-left'));
+		// left control bar
+		this.$element.find('.ui-jqLayout-controlBar-left,.ui-jqLayout-controlBar-left > .ui-jqLayout-controlBut').on('click', function () {
+			if ($(this).hasClass('ui-jqLayout-controlBar-left')) {
+				if($this.options.west.drag){
+					$this.options.west.drag = false;
+					return false;
+				}
+				$this.options.west.auto = true;
+			}else {
+				$this.options.west.auto =false;
+			}
+			privateMembers.toggleWest($this);
 			return false;
 		});
 		this.$element.find('.ui-jqLayout-controlBar-right,' +
 			'.ui-jqLayout-controlBar-right > .ui-jqLayout-controlBut').on('click', function () {
-			privateMembers.toggleEast($this,!$(this).hasClass('ui-jqLayout-controlBar-right'));
+			if ($(this).hasClass('ui-jqLayout-controlBar-right')) {
+				if($this.options.east.drag){
+					$this.options.east.drag = false;
+					return false;
+				}
+				$this.options.east.auto = true;
+			}else {
+				$this.options.east.auto = false;
+			}
+			privateMembers.toggleEast($this);
 			return false;
 		});
-		this.$element.on('mouseenter','.ui-jqLayout-east', function () {
-			if($this.options.east.automaticShrinkage){
-				if (!$this.options.east.isOpen){
+		this.$element.on('mouseenter', '.ui-jqLayout-east', function () {
+			if ($this.options.east.auto) {
+				if (!$this.options.east.open) {
 					let east = $this.$element.find('.ui-jqLayout-east');
 					let bar = $this.$element.find('.ui-jqLayout-controlBar-right');
 					let center = $this.$element.find('.ui-jqLayout-center');
-					east.animate( {width:$this.options.east.size}, 1000 );
-					bar.animate( {right:$this.options.east.size}, 1000 );
-					center.animate( {"padding-right": $this.options.east.size +
-							$this.options.east.barWidth}, 1000 );
-					$this.options.east.isOpen =true;
-				}
-			}
-			return false;
-		});
-		this.$element.on('mouseleave','.ui-jqLayout-east', function () {
-			if($this.options.east.automaticShrinkage){
-				if ($this.options.east.isOpen){
-					let east = $this.$element.find('.ui-jqLayout-east');
-					let bar = $this.$element.find('.ui-jqLayout-controlBar-right');
-					let center = $this.$element.find('.ui-jqLayout-center');
-					east.animate( {width:$this.options.east.closedWidth}, 1000 );
-					bar.animate( {right:$this.options.east.closedWidth}, 1000 );
-					center.animate( {"padding-right": $this.options.east.closedWidth +
-							$this.options.east.barWidth}, 1000 );
-					$this.options.east.isOpen =false;
-				}
-			}
-			return false;
-		});
-		this.$element.on('mouseenter','.ui-jqLayout-west', function () {
-			if($this.options.west.automaticShrinkage){
-				let west = $this.$element.find('.ui-jqLayout-west');
-				let bar = $this.$element.find('.ui-jqLayout-controlBar-left');
-				let center = $this.$element.find('.ui-jqLayout-center');
-				if (!$this.options.west.isOpen) {
-					west.animate( {width:$this.options.west.size}, 1000 );
-					bar.animate({left:$this.options.west.size},1000);
-					center.animate({"padding-left":$this.options.west.size +
-							$this.options.west.barWidth},1000);
-					$this.options.west.isOpen =true;
-				}
-			}
-			return false;
-		});
-		this.$element.on('mouseleave','.ui-jqLayout-west', function () {
-			if($this.options.west.automaticShrinkage) {
-				let west = $this.$element.find('.ui-jqLayout-west');
-				let bar = $this.$element.find('.ui-jqLayout-controlBar-left');
-				let center = $this.$element.find('.ui-jqLayout-center');
-				if ($this.options.west.isOpen) { // 收缩
-					west.animate({width: $this.options.west.closedWidth}, 1000);
-					bar.animate({left: $this.options.west.closedWidth}, 1000);
+					east.animate({width: $this.options.east.size}, 1000);
+					bar.animate({right: $this.options.east.size}, 1000);
 					center.animate({
-						"padding-left": $this.options.west.closedWidth +
-							$this.options.west.barWidth
+						"padding-right": $this.options.east.size +
+							$this.options.east.barSize
 					}, 1000);
-					$this.options.west.isOpen = false;
+					$this.options.east.open = true;
 				}
 			}
 			return false;
 		});
-		$(this.rootClass).tooltip({ show: { effect: "blind", duration: 100,delay:$this.options.toolTipDelay },track: true});
+		this.$element.on('mouseleave', '.ui-jqLayout-east', function () {
+			if ($this.options.east.auto) {
+				if ($this.options.east.open) {
+					let east = $this.$element.find('.ui-jqLayout-east');
+					let bar = $this.$element.find('.ui-jqLayout-controlBar-right');
+					let center = $this.$element.find('.ui-jqLayout-center');
+					east.animate({width: $this.options.east.minSize}, 1000);
+					bar.animate({right: $this.options.east.minSize}, 1000);
+					center.animate({
+						"padding-right": $this.options.east.minSize +
+							$this.options.east.barSize
+					}, 1000);
+					$this.options.east.open = false;
+				}
+			}
+			return false;
+		});
+		this.$element.on('mouseenter', '.ui-jqLayout-west', function () {
+			if ($this.options.west.auto) {
+				let west = $this.$element.find('.ui-jqLayout-west');
+				let bar = $this.$element.find('.ui-jqLayout-controlBar-left');
+				let center = $this.$element.find('.ui-jqLayout-center');
+				if (!$this.options.west.open) {
+					west.animate({width: $this.options.west.size}, 1000);
+					bar.animate({left: $this.options.west.size}, 1000);
+					center.animate({
+						"padding-left": $this.options.west.size +
+							$this.options.west.barSize
+					}, 1000);
+					$this.options.west.open = true;
+				}
+			}
+			return false;
+		});
+		this.$element.on('mouseleave', '.ui-jqLayout-west', function () {
+			if ($this.options.west.auto) {
+				let west = $this.$element.find('.ui-jqLayout-west');
+				let bar = $this.$element.find('.ui-jqLayout-controlBar-left');
+				let center = $this.$element.find('.ui-jqLayout-center');
+				if ($this.options.west.open) { // 收缩
+					west.animate({width: $this.options.west.minSize}, 1000);
+					bar.animate({left: $this.options.west.minSize}, 1000);
+					center.animate({
+						"padding-left": $this.options.west.minSize +
+							$this.options.west.barSize
+					}, 1000);
+					$this.options.west.open = false;
+				}
+			}
+			return false;
+		});
+		$(this.rootClass).tooltip({
+			show: {effect: "blind", duration: 100, delay: $this.options.toolTipDelay},
+			track: true
+		});
+
+		this.$element.find('.ui-jqLayout-controlBar-left').draggable({
+			distance: 10, cursor: "w-resize", axis: "x",
+			containment: "document",
+			cancel:".ui-jqLayout-controlBut",
+			start: function (event, ui) {
+				$this.options.west.drag = true;
+			},
+			drag: function (event, ui) {
+				if (ui.offset.left <= $this.options.west.minSize) {
+					return false;
+				}
+				let center = $this.$element.find('.ui-jqLayout-center');
+				let rightBar = $this.$element.find('.ui-jqLayout-controlBar-right');
+				if(ui.offset.left >= rightBar.offset().left - rightBar.width()){
+					return false;
+				}
+				let west = $this.$element.find('.ui-jqLayout-west');
+				let leftBar = $this.$element.find('.ui-jqLayout-controlBar-left');
+				west.css('width', ui.offset.left + 'px');
+				leftBar.css("left", ui.offset.left + 'px');
+				center.css('padding-left', ui.offset.left + $this.options.west.barSize + 'px');
+				$this.options.west.size = ui.offset.left;
+			}
+		});
+		this.$element.find('.ui-jqLayout-controlBar-right').draggable({
+			distance: 10, cursor: "move", axis: "x",
+			containment: "document",
+			cancel:".ui-jqLayout-controlBut",
+			start: function (event, ui) {
+				$this.options.east.drag = true;
+			},
+			drag: function (event, ui) {
+				// 左边限制
+				if (ui.offset.left < $this.options.west.size + $this.options.west.barSize) {
+					return false;
+				}
+				let east = $this.$element.find('.ui-jqLayout-east');
+				let val = east.offset().left -ui.offset.left - $this.options.east.barSize;
+				// 最小限制
+				if ($this.options.east.size + val <= $this.options.east.minSize) {
+					return false;
+				}
+				$this.options.east.size += val;//screen - ui.offset.left - $this.options.east.barSize;
+				let bar = $this.$element.find('.ui-jqLayout-controlBar-right');
+				let center = $this.$element.find('.ui-jqLayout-center');
+				east.css('width', $this.options.east.size + 'px');
+				bar.css("right", $this.options.east.size + 'px');
+				center.css('padding-right', $this.options.east.size + $this.options.east.barSize + 'px');
+			}
+		});
 	};
 	jqLayout.prototype = {
 		// 切换面板的开启闭合
-		togglePanel: function (panel,automaticShrinkage) {
+		togglePanel: function (panel, automaticShrinkage) {
 			switch (panel) {
 				case "west":
-					return privateMembers.toggleWest($(this),automaticShrinkage);
+					return privateMembers.toggleWest($(this), automaticShrinkage);
 				case "east":
-					return privateMembers.toggleEast($(this),automaticShrinkage);
+					return privateMembers.toggleEast($(this), automaticShrinkage);
 				default:
 					break;
 			}
@@ -112,7 +189,7 @@
 			activeItemStyle: "ui-state-active",
 			contextStyle: "ui-widget-content",
 			hoverStyle: "ui-state-hover",
-			toolTipDelay: 2000,
+			toolTipDelay: 3000,
 			className: "",
 			north: {
 				style: "ui-widget-header",
@@ -124,29 +201,33 @@
 			},
 			west: {
 				style: "ui-widget-content",
-				size: 220,
-				enableBar: true,
-				barWidth: 8,
+				minSize: 0,
+				enableBar: true, // 是否添加drag bar
+				barSize: 8,
 				barButHeight: 50,
-				isOpen: true,
 				barOpenContent: "",
 				barClosedContent: "",
-				barTooltip:"打开/隐藏面板",
-				closedWidth: 50,
-				automaticShrinkage: false
+				barTooltip: "拖拉改变面板大小", // 拖拉bar 提示
+				shrinkBtTip: "折叠/打开面板",   // 收缩按钮提示
+				initClose: false,              // 初始化状态->是否关闭
+				auto: false,                    // 自动收缩 以下是随时可变状态
+				open: true,                    // panel 开启状态
+				size: 220                     // panel大小
 			},
 			east: {
-				style: "ui-widget-content",
-				size: 150,
-				enableBar: true,
-				barWidth: 8,
+				style: "ui-widget-content", // panel相关
+				minSize: 0,
+				enableBar: true,           // bar相关
+				barSize: 8,
 				barButHeight: 50,
-				isOpen: false,
 				barOpenContent: "",
 				barClosedContent: "",
-				barTooltip:"打开/隐藏面板",
-				closedWidth: 0,
-				automaticShrinkage: false
+				barTooltip: "拖拉改变面板大小", // 拖拉bar 提示
+				shrinkBtTip: "折叠/打开面板",   // 收缩按钮提示
+				initClose: true,               // 初始化状态->是否关闭
+				auto: false,                     // 自动收缩 以下是随时可变状态
+				open: true,                     // panel 开启状态
+				size: 150                      // panel 大小
 			},
 			center: {
 				style: "ui-widget-content"
@@ -158,11 +239,13 @@
 			let west = $(element.rootClass + '.ui-jqLayout-west');
 			let east = $(element.rootClass + '.ui-jqLayout-east');
 			let center = $(element.rootClass + '.ui-jqLayout-center');
-			north.addClass(element.options.northStyle);
-			south.addClass(element.options.southStyle);
-			west.addClass(element.options.westStyle);
-			east.addClass(element.options.eastStyle);
-			center.addClass(element.options.centerStyle);
+			// 添加 style
+			north.addClass(element.options.north.style);
+			south.addClass(element.options.south.style);
+			west.addClass(element.options.west.style);
+			east.addClass(element.options.east.style);
+			center.addClass(element.options.center.style);
+			// 重新定位
 			north.css('height', element.options.north.size || 50 + 'px');
 			south.css('height', element.options.south.size || 30 + 'px');
 			let top = 0, bottom = 0;
@@ -181,80 +264,91 @@
 			center.css('bottom', bottom);
 			center.css('padding-top', 0);
 			center.css('padding-bottom', 0);
-			let paddingLeft = element.options.west.isOpen ? element.options.west.size : 0,
-				paddingRight = element.options.east.isOpen ? element.options.east.size : 0;
+			if (element.options.west.initClose) {
+				element.options.west.open = false;
+				west.css('width', 0);
+			}
+			if (element.options.east.initClose) {
+				element.options.east.open = false;
+				east.css('width', 0);
+			}
+			// 添加bar
 			if (element.options.west.enableBar) {
-				paddingLeft += element.options.west.barWidth;
-				// 添加controlBar
-				let leftBar = '<div title="' + element.options.west.barTooltip +
-					'" class="ui-state-default ui-jqLayout-controlBar-left" style="box-sizing: border-box;top:' + top + ';bottom: ' +
-					bottom + ';width: ' + element.options.west.barWidth + 'px;left:' +
-					(element.options.west.isOpen?element.options.west.size:element.options.west.closedWidth) + 'px">\
-				<div title="'+ element.options.west.barTooltip +
-					'" class="ui-state-hover ui-jqLayout-controlBut" style="box-sizing: border-box;height:' +
-					element.options.west.barButHeight + 'px;width: ' +
-					element.options.west.barWidth + 'px;">' +
-					(element.options.west.isOpen ? element.options.west.barOpenContent : element.options.west.barClosedContent) + '</div>\
-				</div>';
+				let leftBar =
+					'<div title="' + element.options.west.barTooltip +
+						'" class="ui-state-default ui-jqLayout-controlBar-left" style="position: absolute;z-index: 668;cursor: e-resize;box-sizing: border-box;top:'
+						+ top + ';bottom: ' + bottom + ';width: ' + element.options.west.barSize + 'px;left:' + element.options.west.size + 'px">\
+						<div title="' + element.options.west.shrinkBtTip +
+							'" class="ui-state-hover ui-jqLayout-controlBut" style="width: 100%;box-sizing: border-box;height:' +
+							element.options.west.barButHeight + 'px;">' +
+							(element.options.west.closed ? element.options.west.barClosedContent : element.options.west.barOpenContent) +
+						'</div>\
+					</div>';
 				element.$element.append(leftBar);
 			}
 			if (element.options.east.enableBar) {
-				paddingRight += element.options.east.barWidth;
-				let rightBar = '<div title="' + element.options.east.barTooltip +
-					'" class="ui-state-default ui-jqLayout-controlBar-right" style="box-sizing: border-box;top:' + top + ';bottom:'
-					+ bottom + ';width: ' + element.options.east.barWidth + 'px;right:' +
-					(element.options.east.isOpen?element.options.east.size:element.options.east.closedWidth) + 'px;">\
-				<div title="' + element.options.east.barTooltip +
-					'" class="ui-state-hover ui-jqLayout-controlBut" style="box-sizing: border-box;height:' +
-					element.options.east.barButHeight + ';width: ' + element.options.east.barWidth + 'px;">' +
-					(element.options.east.isOpen ? element.options.east.barOpenContent : element.options.east.barClosedContent) + '</div>\
-				</div>';
+				let rightBar =
+					'<div title="' + element.options.east.barTooltip +
+					'" class="ui-state-default ui-jqLayout-controlBar-right" style="position: absolute;z-index: 668;cursor: e-resize;box-sizing: border-box;top:' + top + ';bottom:'
+					+ bottom + ';width: ' + element.options.east.barSize + 'px;right:' +
+					(element.options.east.open ? element.options.east.size : element.options.east.minSize) + 'px;">\
+						<div title="' + element.options.east.shrinkBtTip +
+					'" class="ui-state-hover ui-jqLayout-controlBut" style="width: 100%; box-sizing: border-box;height:' +
+					element.options.east.barButHeight + ';">' +
+					(element.options.east.closed ? element.options.east.barClosedContent : element.options.east.barOpenContent) +
+					'</div>\
+					</div>';
 				element.$element.append(rightBar);
 			}
-			west.css('width', paddingLeft - element.options.west.barWidth + 'px');
-			east.css('width', paddingRight - element.options.east.barWidth  + 'px');
-			center.css('padding-left', paddingLeft);
-			center.css('padding-right', paddingRight);
+			center.css('padding-left', element.options.west.barSize +  (element.options.west.initClose?0:element.options.west.size));
+			center.css('padding-right', element.options.east.barSize + (element.options.east?0:element.options.east.size));
 		},
-		toggleWest:function (element,automaticShrinkage) {
+		toggleWest: function (element) {
 			let west = element.$element.find('.ui-jqLayout-west');
 			let bar = element.$element.find('.ui-jqLayout-controlBar-left');
 			let center = element.$element.find('.ui-jqLayout-center');
-			if (element.options.west.isOpen) { // 收缩
-				west.animate( {width:element.options.west.closedWidth}, 1000 );
-				bar.animate({left:element.options.west.closedWidth},1000);
-				center.animate({"padding-left":element.options.west.closedWidth +
-						element.options.west.barWidth},1000);
-				element.options.west.isOpen =false;
+			if (element.options.west.open) { // 收缩
+				west.animate({width: element.options.west.minSize}, 1000);
+				bar.animate({left: element.options.west.minSize}, 1000);
+				center.animate({
+					"padding-left": element.options.west.minSize +
+						element.options.west.barSize
+				}, 1000);
+				element.options.west.open = false;
 			} else { // 打开
-				west.animate( {width:element.options.west.size}, 1000 );
-				bar.animate({left:element.options.west.size},1000);
-				center.animate({"padding-left":element.options.west.size +
-						element.options.west.barWidth},1000);
-				element.options.west.isOpen =true;
+				west.animate({width: element.options.west.size}, 1000);
+				bar.animate({left: element.options.west.size}, 1000);
+				center.animate({
+					"padding-left": element.options.west.barSize +
+						element.options.west.size
+				}, 1000);
+				element.options.west.open = true;
 			}
-			element.options.west.automaticShrinkage = automaticShrinkage;
-			return element.options.west.isOpen;
+			return element.options.west.open;
 		},
-		toggleEast:function (element,automaticShrinkage) {
+		toggleEast: function (element) {
 			let east = element.$element.find('.ui-jqLayout-east');
 			let bar = element.$element.find('.ui-jqLayout-controlBar-right');
 			let center = element.$element.find('.ui-jqLayout-center');
-			if (element.options.east.isOpen){
-				east.animate( {width:element.options.east.closedWidth}, 1000 );
-				bar.animate( {right:element.options.east.closedWidth}, 1000 );
-				center.animate( {"padding-right": element.options.east.closedWidth +
-						element.options.east.barWidth}, 1000 );
-				element.options.east.isOpen =false;
-			}else{
-				east.animate( {width:element.options.east.size}, 1000 );
-				bar.animate( {right:element.options.east.size}, 1000 );
-				center.animate( {"padding-right": element.options.east.size +
-						element.options.east.barWidth}, 1000 );
-				element.options.east.isOpen =true;
+			bar.css("left", "auto");
+			if (element.options.east.open) {
+				east.animate({width: element.options.east.minSize}, 1000);
+				bar.animate({right: element.options.east.minSize}, 1000);
+				center.animate({
+					"padding-right": element.options.east.minSize +
+						element.options.east.barSize
+				}, 1000);
+				element.options.east.open = false;
+			} else {
+				east.animate({width: element.options.east.size}, 1000);
+				bar.animate({right: element.options.east.size}, 1000);
+				center.animate({
+					"padding-right": element.options.east.size +
+						element.options.east.barSize
+				}, 1000);
+				element.options.east.open = true;
 			}
-			element.options.east.automaticShrinkage = automaticShrinkage;
-			return element.options.east.isOpen;
+			return element.options.east.open;
 		}
 	};
 	$.fn.jqLayout = function (option) {
